a360858da3e2f56e524f9f1bb1434c017511779e
course2025_17 <gamesconsole1104@gmail.com>
2025-11-30 10:10:59 +0500

    Из сунца

---
+++ Тема 3/C.py
@@ -0,0 +1,8 @@
+a = int(input())
+if a > 0:
+    x = 1
+elif a < 0:
+    x = -1
+elif a == 0:
+    x = 0
+print(x)
---
+++ Тема 4/F.py
@@ -0,0 +1,6 @@
+def fac(k):
+	n = 1
+	for i in range(k+1):
+		if i != 0:
+			n = n * i
+	return(n)

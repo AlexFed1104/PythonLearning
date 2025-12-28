94d86ce422f048b8e224f5557372061e67206fbf
course2025_17 <gamesconsole1104@gmail.com>
2025-12-28 11:10:53 +0500

    Revert "+"
    This reverts commit 6dee93a86f733b0e4ea6f9afcd597022c3e6b9be.

--- Другие задачи/Задачи с доски/Алгоритмы Евклида/НОД.py
+++
@@ -1,9 +0,0 @@
-def NOD(a, b):
-	x = a
-	y = b
-	while x != y:
-		if x > y:
-			x = x - y
-		elif x < y:
-			y = y - x
-	return x
--- Другие задачи/Задачи с доски/Алгоритмы Евклида/НОК.py
+++
@@ -1,5 +0,0 @@
-def NOK(q, w):
-	from НОД import NOD
-	x = NOD(q, w)
-	y = q * w
-	print(int(y / x))
